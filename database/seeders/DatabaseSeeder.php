2d6e224f27645a851f6ec5f8883b2da1dfb17c21
fliegenhaan <perdanaraihan96@gmail.com>
2025-08-22 09:42:42 +0700

    feat: add migrations and seeders for quiz

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CourseSeeder::class,
             ModuleSeeder::class,
+            QuizSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/QuizSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Module;
+use App\Models\Quiz;
+use App\Models\Question;
+use App\Models\Answer;
+
+class QuizSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $modules = Module::all();
+
+        if ($modules->isEmpty()) {
+            $this->command->info('Tidak ada modul untuk ditambahkan kuis. Silakan jalankan CourseSeeder terlebih dahulu.');
+            return;
+        }
+
+        $this->command->info('Membuat kuis untuk beberapa modul...');
+
+        foreach ($modules as $module) {
+            if ($module->id % 2 == 0) {
+                $quiz = Quiz::create([
+                    'module_id' => $module->id,
+                    'title' => 'Kuis Pemahaman: ' . $module->title,
+                    'description' => 'Uji pemahaman Anda tentang materi yang telah dipelajari di modul ini.',
+                    'passing_score' => 75,
+                ]);
+
+                for ($i = 1; $i <= 3; $i++) {
+                    $question = Question::create([
+                        'quiz_id' => $quiz->id,
+                        'question_text' => "Manakah pernyataan yang paling tepat mengenai konsep #$i di modul '{$module->title}'?",
+                    ]);
+
+                    Answer::create([
+                        'question_id' => $question->id,
+                        'answer_text' => 'Ini adalah jawaban yang benar untuk pertanyaan ' . $i . '.',
+                        'is_correct' => true,
+                    ]);
+
+                    for ($j = 1; $j <= 3; $j++) {
+                        Answer::create([
+                            'question_id' => $question->id,
+                            'answer_text' => "Ini adalah pilihan jawaban salah #$j.",
+                            'is_correct' => false,
+                        ]);
+                    }
+                }
+            }
+        }
+        
+        $this->command->info('Seeder kuis berhasil dijalankan!');
+    }
+}
