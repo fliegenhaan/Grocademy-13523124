07cc9f3bd98f10516d02e2809ec03c0f315fb59d
fliegenhaan <perdanaraihan96@gmail.com>
2025-08-23 17:52:10 +0700

    refactor: add hasfactory for answer,question, and quiz

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
+    use HasFactory;
+
     public function module()
     {
         return $this->belongsTo(Module::class);
